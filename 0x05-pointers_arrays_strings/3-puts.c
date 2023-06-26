c0e75917a8762088b6b6e41b1955dc4dd2615187
wairimu5 <wairimu5>
2023-06-26 15:24:51 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: Pointer to a string.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+}
+
+
