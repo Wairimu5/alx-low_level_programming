babba1479933e16dac58c191c9e5e8fe7e45faa3
wairimu5 <wairimu5>
2023-07-06 12:07:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              using the puts function.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
