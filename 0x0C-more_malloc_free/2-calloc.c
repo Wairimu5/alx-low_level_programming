88faf7ff6f10ccf804b55558a440071c799e0157
wairimu5 <wairimu5>
2023-07-13 13:02:58 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero
+ * @nmemb: The number of elements in the array
+ * @size: The size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory
+ *         NULL if nmemb or size is 0 or if malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, total_size);
+
+	return (ptr);
+}
