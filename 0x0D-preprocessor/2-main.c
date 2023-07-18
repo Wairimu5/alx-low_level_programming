949230b71ba4556f263ba13b476db371dfef8791
wairimu5 <wairimu5>
2023-07-18 14:22:26 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    /* Your code here */
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
