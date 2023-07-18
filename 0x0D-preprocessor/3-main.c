08f24d1b320e8cf930d7afb53c23dbe989e3395a
wairimu5 <wairimu5>
2023-07-18 14:55:07 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int i;
-    int j;
+    int num = -5;
+    int absValue = ABS(num);
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
+    printf("The absolute value of %d is %d\n", num, absValue);
+
     return (0);
 }
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -2,5 +2,6 @@
 #define MYHEADER_H
 
 #define SIZE 1024
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif /* MYHEADER_H */
