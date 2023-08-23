2681c16d00ac75c19024c7a6bbc3814f670c0b7c
Wairimu5 <innomjue@gmail.com>
2023-08-23 14:49:48 -0700

    0-read_textfile.c update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,69 @@
-#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
- * Return: w - Actual number of bytes read and printed,
- *         0 when function fails or filename is NULL.
+ * _strlen - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t _strlen(char *s)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int len;
+	int i;
 
+	len = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+
+/**
+ * read_textfile - set the integer to 402
+ * @filename: a pointer the integer we want to set to 98
+ * @letters : number of letters to be printed and read
+ * Return: the number of letters it could read and print
+ */
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, check;
+	char *file_content;
+
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-	free(buf);
+	file_content = malloc(sizeof(char) * (letters + 1));
+	if (file_content == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	check = read(fd, file_content, letters);
+	if (check == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	*(file_content + letters) = '\0';
+	if (letters > _strlen(file_content))
+		check = write(STDOUT_FILENO, file_content, _strlen(file_content));
+	else
+		check = write(STDOUT_FILENO, file_content, letters);
+	if (check == -1)
+	{
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	return (w);
+	return (check);
 }
-
