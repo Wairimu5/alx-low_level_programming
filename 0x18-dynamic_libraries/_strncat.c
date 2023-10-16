1a103c28878d2c5e88f9e1999cf8b2261ad24be0
Wairimu5 <innonjue@gmail.com>
2023-10-16 14:51:01 -0700

    done

---
+++ 0x18-dynamic_libraries/_strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat - set the integer to 402
+ * @dest: a pointer the integer we want to set to 98
+ * @src: the string to be concatenated
+ * @n : letter being tested
+ * Return: nothing
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (NULL);
+}
---
+++ 0x18-dynamic_libraries/_strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strpbrk - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ * @accept : letter being tested
+ * Return: nothing
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (NULL);
+}
